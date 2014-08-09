f0572f84ed824278191245459bd1540297624558
Maspik <maspik.now@gmail.com>
2014-08-10 00:42:16 +0100

    Replacing iframe src with srcdoc to enable automatic detection of full asset urls.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,5 +1,43 @@
 'use strict';
 
+var overlayContent = "<head>\
+	                     <title></title>\
+\
+	                     <link rel='stylesheet' type='text/css' href='{{STYLE_SHEET}}'/>\
+\
+						<script>var frameIndex = {{INDEX}};\
+						</script>\
+\
+	                     <script src='{{JQUERY_URL}}'></script>\
+	                     \
+	                     <script src='{{FIT_TEXT_URL}}'></script>\
+\
+                     </head>\
+                     \
+                     <div id='overlayPanel'>\
+	                     <div id='header'>\
+		                     <h1>Maspik</h1>\
+		                     <div id='closePositioner'>\
+			                     <button id='closeButton'>x</button>\
+		                     </div>\
+\
+	                     </div>\
+	                     <p>You might not want to view this as:</p>\
+\
+	                    {{REASON}}\
+	                    \
+	                     <img class='peace' border='0' src='{{PEACE_URL}}'/>\
+\
+	                     <script src='{{OVERLAY_JS_URL}}'></script>\
+                     </div>";
+
+
+var overlayStyleSheet = chrome.extension.getURL( "styles/overlay.css" );
+var jqueryUrl = chrome.extension.getURL( "scripts/libs/jquery-2.1.1.min.js" );
+var fitTextUrl = chrome.extension.getURL( "scripts/libs/jquery.fittext.js" );
+var peaceUrl = chrome.extension.getURL( "images/peace.svg" );
+var overlayJSUrl = chrome.extension.getURL( "scripts/overlay.js" );
+
 window.addEventListener( "message", function( event ) {
 
 	var data = event.data;
@@ -51,12 +89,21 @@ function findAndObscure( widgets, filter ) {
 			iframe.scrolling = "no";
 			iframe.setAttribute( "frameBorder", "0" );
 			iframe.setAttribute( "allowtransparency", "true" );
-			iframe.src = chrome.extension.getURL( 'pages/overlay.html?filter=' + filter + '&index=' + index );
+
+			var frameContent = overlayContent.replace( /{{REASON}}/, filters[ filter ].reason );
+			frameContent = frameContent.replace( /{{STYLE_SHEET}}/, overlayStyleSheet );
+			frameContent = frameContent.replace( /{{JQUERY_URL}}/, jqueryUrl );
+			frameContent = frameContent.replace( /{{FIT_TEXT_URL}}/, fitTextUrl );
+			frameContent = frameContent.replace( /{{PEACE_URL}}/, peaceUrl );
+			frameContent = frameContent.replace( /{{OVERLAY_JS_URL}}/, overlayJSUrl );
+			frameContent = frameContent.replace( /{{INDEX}}/, index );
+			iframe.srcdoc = frameContent;
 
 			$( iframe ).appendTo( wrapper );
 		}
 	} );
 }
+
 
 
 function getTotalHeight( target ) {
--- app/scripts/overlay.js
+++ app/scripts/overlay.js
@@ -1,28 +1,9 @@
-function getUrlParameter( param ) {
-
-	var pageURL = window.location.search.substring( 1 );
-	var URLVariables = pageURL.split( '&' );
-	for ( var i = 0; i < URLVariables.length; i++ ) {
-
-		var parameterName = URLVariables[ i ].split( '=' );
-		if ( parameterName[ 0 ] === param ) {
-
-			return parameterName[ 1 ];
-		}
-	}
-	return '';
-}
-
 function closeWindow(){
 
-	window.top.postMessage( { origin : 'maspik', message :'close', index : getUrlParameter( 'index' ) }, '*' );
+	window.top.postMessage( { origin : 'maspik', message :'close', index : frameIndex }, '*' );
 }
 
-var filter = getUrlParameter( 'filter' );
-
 $( "#header" ).fitText( 1, { minFontSize:'6px', maxFontSize : '15px' } );
-
-$( '#reason' ).append( filters[ filter ].reason );
 
 $( '#closeButton' ).click(closeWindow );
 
